272a1e9a88faab2442e2803a4e3ef0df9fdde61a
Nick_tsygankov <kolanberat@gmail.com>
2024-06-19 23:55:29 +0300

    Rename operand

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args)
     {
-        int[] Array = new int[]{-132231, 6789, -1, 32230, 6, -426460, -51434, -11110, -10010, -3210, 426459};
-        MySearch search = new MySearch(Array);
+        int[] array = new int[]{-132231, 6789, -1, 32230, 6, -426460, -51434, -11110, -10010, -3210, 426459};
+        MySearch search = new MySearch(array);
 
         //1. Ищем Максимально и Минимальное число
-        search.Extremum();
+        search.extremum();
 
         //2. Выведем четные и нечетные числа массива
-        search.Even_ornot();
+        search.evenOrNot();
 
         //3. Выведем самое длинное и короткое число массива
-        search.Longer_orShorter();
+        search.longerOrShorter();
 
         //4. Выведем те числа массива в которых цифры идут в порядке возрастания
-        search.Num_Sequence();
+        search.numSequence();
 
         //5. Выведем числа являющиеся Палиндромами
-        search.Palendrome();
+        search.palendrome();
     }
 }
--- src/MySearch.java
+++ src/MySearch.java
@@ -1,93 +1,93 @@
 public class MySearch {
-    int[] Array;
+    int[] array;
     MySearch (int[] _Array){
-        Array = _Array;
+        array = _Array;
     }
-    void Extremum(){
-        int Max = Array[0];
-        int Min = Array[0];
-        for (int i =0; i < Array.length; i++){
-            if (Min > Array[i]){
-                Min = Array[i];
+    void extremum(){
+        int max = array[0];
+        int min = array[0];
+        for (int i = 0; i < array.length; i++){
+            if (min > array[i]){
+                min = array[i];
             }
-            if (Max < Array[i]){
-                Max = Array[i];
+            if (max < array[i]){
+                max = array[i];
             }
         }
-        System.out.println("Максимальное число массива : "+Max);
-        System.out.println("Минимальное число массива : "+Min);
+        System.out.println("Максимальное число массива : "+ max);
+        System.out.println("Минимальное число массива : "+ min);
     }
-    void Even_ornot(){
+    void evenOrNot(){
         System.out.print("Четные числа массива : ");
-        for (int i =0; i < Array.length; i++){
-            if (Array[i] % 2 == 0){
-                System.out.print(Array[i]+" ");
+        for (int i = 0; i < array.length; i++){
+            if (array[i] % 2 == 0){
+                System.out.print(array[i]+" ");
             }
         }
         System.out.println(); // выведем нечетные с новой строки
 
         System.out.print("Нечетные числа массива : ");
-        for (int i =0; i < Array.length; i++){
-            if (Array[i] % 2 != 0){
-                System.out.print(Array[i]+" ");
+        for (int i = 0; i < array.length; i++){
+            if (array[i] % 2 != 0){
+                System.out.print(array[i]+" ");
             }
         }
         System.out.println();
     }
-    void Num_Sequence(){
+    void numSequence(){
         System.out.print("Последовательные числа: ");
-        for (int i =0; i < Array.length; i++) {
-            int future=Array[i]/10; // Переменная которая будет на шаг впереди=)
+        for (int i = 0; i < array.length; i++) {
+            int future= array[i]/10; // Переменная которая будет на шаг впереди=)
             int count=1;
             int newCount=1;
             if (future == 0){ //пропустим натуральные числа
                 continue;
             }
-            for (int present = Array[i]; future != 0; present /= 10, future /= 10,count++) {
+            for (int present = array[i]; future != 0; present /= 10, future /= 10,count++) {
 
                 if (present % 10 == future % 10 +1){ //сравниваем остатки переменных настоящего и будущего добавляя шаг+1 =)
                     newCount++;
                 }
             }
             if (count == newCount){ //Если кол-во выполнений цикла for равно количеству выполненных условий if наше число последовательно  \^,^/
-                System.out.print(Array[i]+" ");
+                System.out.print(array[i]+" ");
             }
         }
         System.out.println();
     }
 
-    void Longer_orShorter(){
-        int Longer =Array[0] <0 ? Array[0]*-1 : Array[0];
-        int Short =Array[0] <0 ? Array[0]*-1 : Array[0];
+    void longerOrShorter(){
+        int longer = array[0] <0 ? array[0]*-1 : array[0];
+        int shorter = array[0] <0 ? array[0]*-1 : array[0];
         int numLong =0;
         int numShort =0;
-        for (int i =0, temp; i < Array.length; i++) {
-            temp = Array[i]<0 ? Array[i]*-1 : Array[i];
-            if (Longer < temp){
-                Longer = temp;
+        for (int i = 0, temp; i < array.length; i++) {
+            temp = array[i]<0 ? array[i]*-1 : array[i];
+            if (longer < temp){
+                longer = temp;
                 numLong =i;
             }
-            if (Short > temp){
-                Short = temp;
+            if (shorter > temp){
+                shorter = temp;
                 numShort =i;
             }
         }
-        System.out.println("Самое длинное число массива : "+Array[numLong]);
-        System.out.println("Самое короткое число массива : "+Array[numShort]);
+        System.out.println("Самое длинное число массива : "+ array[numLong]);
+        System.out.println("Самое короткое число массива : "+ array[numShort]);
     }
 
-    void Palendrome(){
+    void palendrome(){
         System.out.print("Числа массива являющиеся Палиндромами : ");
-        for (int i =0, count =0; i< Array.length; i++){
+        for (int i = 0, count = 0; i< array.length; i++){
             int buff =0;
-            for (int temp =Array[i]; temp != 0; temp /= 10){
+            for (int temp = array[i]; temp != 0; temp /= 10){
                 buff=buff*10+temp%10; //собираю число в обратном порядке
             }
-            if (Array[i] == buff){
+            if (array[i] == buff){
                 count++;
-                System.out.print(Array[i]+" ");
+                System.out.print(array[i]+" ");
             }
-            else if (i == Array.length-1 && count == 0) {
+            else if (i == array.length-1 && count == 0) {
                 System.out.println("Чисел Палиндромов в массиве не найдено =(");
             }
         }
